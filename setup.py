8c94032c7660d499e599b1b13a38c6203678c5fa
Brewster Malevich <sbmalev@gmail.com>
2018-01-17 09:21:43 -0700

    Add basic setup.py and initial package module dirs.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deltaoxfox',
+    version='0.0.1',
+    description='δ18Oc calibration using Bayesian regression',
+    license='GPLv3',
+
+    author='S. Brewster Malevich',
+    author_email='malevich@email.arizona.edu',
+    url='https://github.com/brews/deltaoxfox',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='marine paleoclimate d18O',
+
+    packages=find_packages(exclude=['docs']),
+
+    install_requires=['numpy', 'scipy', 'matplotlib', 'attrs'],
+    tests_require=['pytest'],
+)
