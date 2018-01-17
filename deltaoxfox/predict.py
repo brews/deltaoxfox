7aadc2b559bc67f2c2e70817e6c17adb089cec92
Brewster Malevich <sbmalev@gmail.com>
2018-01-17 12:55:39 -0700

    Fill out prelim predict_d18oc function.

--- deltaoxfox/predict.py
+++ deltaoxfox/predict.py
@@ -18,11 +18,46 @@ def predict_d18osw(salinity, latlon):
     return y
 
 
-def predict_d18oc(seatemp, d18osw=None, salinity=None, latlon=None):
+def predict_d18oc(seatemp, spp, d18osw=None, salinity=None, latlon=None):
     """Predict d18O of calcite given seawater temperature and seawater d18O
     """
+    d18oc_modelparams = get_draws('d18oc')
+    assert spp in list(d18oc_modelparams.alpha.columns)
     assert (d18osw is not None) or ((salinity is not None) and (latlon is not None))
-    raise NotImplementedError
+
+    d18osw_adj = None
+    d18osw_alpha = None
+    d18osw_beta = None
+    d18osw_tau2 = None
+
+    if d18osw is None:
+        modelparams = get_draws('d18osw')
+        d18osw_alpha, d18osw_beta, d18osw_tau2 = modelparams.find_abt2_near(latlon[0], latlon[1])
+        # We're assuming that model parameter draws for d18osw & d18oc model are
+        # the same (for speed)... May change this later with more coding.
+        assert d18oc_modelparams.tau2.size == d18osw_tau2.size
+    else:
+        # Unit adjustment.
+        d18osw_adj = d18osw - 0.27
+
+    y = np.empty((len(seatemp), d18oc_modelparams.tau2.size))
+
+    for i in range(d18oc_modelparams.tau2.size):
+        tau2_now = d18oc_modelparams.tau2.loc[i, spp]
+        beta_now = d18oc_modelparams.beta.loc[i, spp]
+        alpha_now = d18oc_modelparams.alpha.loc[i, spp]
+
+        if d18osw is None:
+            beta_d18osw_now = d18osw_beta[i]
+            alpha_d18osw_now = d18osw_alpha[i]
+            tau2_d18osw_now = d18osw_tau2[i]
+            d18osw_now = np.random.normal(salinity * beta_d18osw_now + alpha_d18osw_now,
+                                          np.sqrt(tau2_d18osw_now))
+            d18osw_adj = d18osw_now - 0.27
+
+        y[:, i] = np.random.normal(alpha_now + seatemp * beta_now + d18osw_adj,
+                                   np.sqrt(tau2_now))
+    return y
 
 
 def predict_seatemp(d18oc, d18osw=None, salinity=None, latlon=None):
