6a7948d37baa60833863ee7a893c1510474ae094
Brewster Malevich <sbmalev@gmail.com>
2018-10-10 11:55:35 -0700

    Release v0.0.1a1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='deltaoxfox',
-    version='0.0.1a0',
+    version='0.0.1a1',
     description='δ18Oc calibration using Bayesian regression',
     long_description=readme(),
     long_description_content_type="text/markdown",
